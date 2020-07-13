27bffd76adece4d5161b699d04adc93c94970ca6
Omar Saucedo <omar.saucedo@wizeline.com>
2020-07-13 15:04:46 -0500

    ExpensesSummary with h1 instead p tag

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,16 +4,17 @@ import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => (
-  <div>
-    <p>
-      Viewing&nbsp;
-      {expenseCount}&nbsp;
-      {expenseCount === 1 ? 'expense' : 'expenses'} totalling&nbsp;
-      {numeral(expensesTotal / 100).format('$0,0.00')}
-    </p>
-  </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div>
+      <h1>
+        Viewing {expenseCount} {expenseWord} totalling {formattedTotal}
+      </h1>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const expenses = selectExpenses(state.expenses, state.filters);
