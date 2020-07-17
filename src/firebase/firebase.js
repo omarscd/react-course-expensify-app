1b25a901bfe0d7fc123f6bcfd913099f67a3d1ff
Omar Saucedo <omar.saucedo@wizeline.com>
2020-07-16 19:30:58 -0500

    setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,34 +1,37 @@
 import * as firebase from 'firebase';
-import moment from 'moment';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBAFZIA0cJwEYqGtMjZO-gv9qcMwMG8v0A',
-  authDomain: 'expensify-9d417.firebaseapp.com',
-  databaseURL: 'https://expensify-9d417.firebaseio.com',
-  projectId: 'expensify-9d417',
-  storageBucket: 'expensify-9d417.appspot.com',
-  messagingSenderId: '892545057156',
-  appId: '1:892545057156:web:ce70796ce0335b4c40ddd1',
-  measurementId: 'G-PNRTLFKLPW'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STOREAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-const expensesRef = database.ref('expenses');
+export {
+  firebase,
+  database as default
+};
 
-expensesRef.on('child_removed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+// const expensesRef = database.ref('expenses');
 
-expensesRef.on('child_changed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+// expensesRef.on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-expensesRef.on('child_added', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+// expensesRef.on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// expensesRef.on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
 // expensesRef.on('value', (snapshot) => {
 //   const expenses = [];
